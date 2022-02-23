a9fd899cf341f201bee6bf702f196916b70d8a1d
aapartyka <76936962+aapartyka@users.noreply.github.com>
2022-02-23 16:13:28 +0100

    Create model.js

---
+++ models.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema( {
+    Title: {type: String, required: true},
+    Description: {type: String, required: true},
+    Genre: {
+        Name: String,
+        Description: String
+    },
+    Director: {
+        Name: String,
+        Bio: String,
+        Birthyear: Date,
+        Deathyear: Date
+    },
+    //Add Actors
+    Imagepath: String,
+    Featured: Boolean
+});
+
+let userSchema = moongose.Schema( {
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
+let Movie = mongoose.Schema('Movie', movieSchema);
+let User = mongoose.Schema('User', userSchema);
+
+modules.exports.Movie = Movie;
+modules.exports.User = User;
