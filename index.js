ef620d802ac8f7b00711cd2366526e475f800cb8
aapartyka <76936962+aapartyka@users.noreply.github.com>
2022-02-27 23:40:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,90 +21,6 @@ const Users = Models.user;
 
 mongoose.connect('mongodb://localhost:27017/myFix', {useNewUrlParser: true, useUnifiedToplogy: true});
 
-// array with the user and their information.
-let users = [
-    {
-        id: 1,
-        name: 'Arno',
-        favoriteMovies: ['Shutter Island']
-    },
-    {
-        id: 2,
-        name: 'Blaise',
-        favoriteMovies: ['We will never know']
-    },
-    {
-        id: 3,
-        name: 'Ramadhan',
-        favoriteMovies: ['Maybe I ask him later']
-    }
-]
-
-// TODO: add rating, release year, artist and short description.
-// Array which contains the movies and their information.
-let movies = [
-    {
-        Title: 'Shutter Island',
-        Genre: 'Thriller',
-        Director: 'Martin Scorsese',
-        Description: 'Add description here :D1',
-    },
-    {
-        Title: 'Ex Machina',
-        Genre: 'testGenre',
-        Director: 'Alex Garland',
-        Description: 'Add description here :D2'
-    },
-    {
-        Title: 'The Dark Knight',
-        Genre: 'testGenre',
-        Director: 'Christopher Nolan',
-        Description: 'Add description here :D3'
-    },
-    {
-        Title: 'The Shawshank Redemption',
-        Genre: 'testGenre',
-        Director: 'Stephen King',
-        Description: 'Add description here :D4'
-    },
-    {
-        Title: 'Pulp Fiction',
-        Genre: 'testGenre',
-        Director: 'Quentin Tarantino',
-        Description: 'Add description here :D5'
-    },
-    {
-        Title: 'The Pianist',
-        Genre: 'testGenre',
-        Director: 'Roman Polanski',
-        Description: 'Add description here :D6'
-    },
-    {
-        Title: 'Lord of the Rings The Fellowship of the Ring',
-        Genre: 'testGenre',
-        Director: 'Peter Jackson',
-        Description: 'Add description here :D7'
-    },
-    {
-        Title: 'Forrest Gump',
-        Genre: 'testGenre',
-        Director: 'Robert Zemeckis',
-        Description: 'Add description here :D8'
-    },
-    {
-        Title: 'The Godfather',
-        Genre: 'testGenre',
-        Director: 'Francis Ford Coppola',
-        Description: 'Add description here :D9'
-    },
-    {
-        Title: 'Star Wars V - The Empire Strikes back',
-        Genre: 'testGenre',
-        Director: 'Irvin Kershner',
-        Description: 'Add description here :D10'
-    }
-];
-
 // Routing.
 //Create: new User
 app.post('/users', (req, res) => {
@@ -136,7 +52,7 @@ app.post('/users', (req, res) => {
 // CREATE: Create new favorite movie of a user.
 app.post('/users/:id/:movieTitle', (req, res) => {
     const { id, movieTitle } = req.params;
-    let user = users.find( user => user.id == id );
+    let user = Users.find( user => user.id == id );
 
     if (user) {
         user.favoriteMovies.push(movieTitle);
@@ -156,13 +72,13 @@ app.use(express.static('public'));
 
 // READ: Get all movies
 app.get('/movies', (req, res) => {
-    res.status(200).json(movies);
+    res.status(200).json(Movies);
 })
 
 // READ: Get movies by title.
 app.get('/movies/:title', (req, res) => {
     const { title } = req.params;
-    const movie = movies.find ( movie => movie.Title === title );
+    const movie = Movies.find ( movie => movie.Title === title );
 
     if (movie) {
         res.status(200).json(movie);
@@ -174,7 +90,7 @@ app.get('/movies/:title', (req, res) => {
 // READ: Get movies by genre.
 app.get('/movies/genres/:genreName', (req, res) => {
     const { genreName } = req.params;
-    const genre = movies.find ( movie => movie.Genre === genreName);
+    const genre = Movies.find ( movie => movie.Genre === genreName);
 
     if (genre) {
         res.status(200).json(genre);
@@ -207,12 +123,10 @@ app.get('/users/:Username', (req, res) => {
       });
   });
 
-
 // READ: Get movies by director.
 app.get('/movies/directors/:directorName', (req, res) => {
-    console.log('test');
     const { directorName } = req.params;
-    const director = movies.find ( movie => movie.Director === directorName );
+    const director = Movies.find ( movie => movie.Director === directorName );
 
     if (director) {
         res.status(200).json(director);
@@ -225,7 +139,7 @@ app.get('/movies/directors/:directorName', (req, res) => {
 app.put('/users/:id', (req, res) => {
     const { id } = req.params;
     const updatedUser = req.body;
-    let user = users.find(user => user.id == id);
+    let user = Users.find(user => user.id == id);
 
     if (user) {
         user.name = updatedUser.name;
@@ -239,10 +153,10 @@ app.put('/users/:id', (req, res) => {
 // DELETE: Delete user.
 app.delete('/users/:id', (req, res) => {
     const { id } = req.params;
-    let user = users.find(user => user.id == id);
+    let user = Users.find(user => user.id == id);
 
     if (user) {
-        users = users.filter(user => user.id != id)
+        Users = user.filter(user => user.id != id)
         // status code 200 = OK
         res.status(200).send(`${user.name} has been deleted`);
     } else {
@@ -253,7 +167,7 @@ app.delete('/users/:id', (req, res) => {
 // DELETE: Delete/remove movie from the user's favorites list.
 app.delete('/users/:id/:movieTitle', (req, res) => {
     const { id, movieTitle } = req.params;
-    let user = users.find(user => user.id == id);
+    let user = Users.find(user => user.id == id);
 
     if (user) {
         user.toWatch = user.toWatch.filter(title => title !==  movieTitle);
