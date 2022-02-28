4c2a20c985cd5727810cb568d7fe49ea10f9dfd8
aapartyka <76936962+aapartyka@users.noreply.github.com>
2022-02-28 14:18:50 +0100

    Update models.js

--- models.js
+++ models.js
@@ -1,33 +1,31 @@
 const mongoose = require('mongoose');
 
-let movieSchema = mongoose.Schema( {
+let movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
     Description: {type: String, required: true},
     Genre: {
-        Name: String,
-        Description: String
+      Name: String,
+      Description: String
     },
     Director: {
-        Name: String,
-        Bio: String,
-        Birthyear: Date,
-        Deathyear: Date
+      Name: String,
+      Bio: String
     },
-    //Add Actors
-    Imagepath: String,
+    // Add Actors: [String],
+    ImagePath: String,
     Featured: Boolean
-});
-
-let userSchema = mongoose.Schema( {
+  });
+  
+  let userSchema = mongoose.Schema({
     Username: {type: String, required: true},
     Password: {type: String, required: true},
     Email: {type: String, required: true},
     Birthday: Date,
-    FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
-});
-
-let Movie = mongoose.Schema('Movie', movieSchema);
-let User = mongoose.Schema('User', userSchema);
-
-modules.exports.Movie = Movie;
-modules.exports.User = User;
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  });
+  
+  let Movie = mongoose.model('Movie', movieSchema);
+  let User = mongoose.model('User', userSchema);
+  
+  module.exports.Movie = Movie;
+  module.exports.User = User;
