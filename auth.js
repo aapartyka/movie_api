77f7c0a8a7cd90eadeb262e16c6e6d8c52bddebf
aapartyka <76936962+aapartyka@users.noreply.github.com>
2022-03-01 15:43:14 +0100

    update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,6 @@
 const jwtSecret = 'your_jwt_secret'; // Same key as used in the JWTStrategy.
 
+const req = require('express/lib/request');
 const jwt = require('jsonwebtoken'),
     passport = require('passport');
 
@@ -8,7 +9,28 @@ require('./passport'); // passport.js with HTTP- and JWT-Strategy.
 let generateJWTToken = (user) => {
     return jwt.sign(user, jwtSecret, {
         subject: user.Username, // Username which will be encoded in JWT.
-        expiresIn: '5d', // Specifies the expiration of the JWT token to 5 days.
+        expiresIn: '6d', // Specifies the expiration of the JWT token to 6 days.
         algorithm: 'HS256' // The HMAC (keyed-hash message authentication code) which is used to "sign"/encode the values of the JWT token.
+    });
+}
+
+/* POST login. */
+module.exports = (router) => {
+    router.post('/login', req, res => {
+        passport.authenticate('local', {session: false}, (error, user, info) => {
+            if (error || !user) {
+                return res.status(400).json({
+                    message: 'Authentication failed, something is not right.',
+                    user: user
+                });
+            }
+            req.login(user, {session: false}, (error) => {
+                if (error) {
+                    res.send(error);
+                }
+                let token = generateJWTToken(user.toJSON());
+                return res.json({ user, token}); // ES6 shorthand for res.json({ user: user, token: token }).
+            });
+        })(req, res);
     });
 }
