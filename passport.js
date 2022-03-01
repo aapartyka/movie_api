f2a841547605cdbbca80def2d375d82891f60e74
aapartyka <76936962+aapartyka@users.noreply.github.com>
2022-03-01 12:11:22 +0100

    Update passport.js
    Define two passport strategies, basic HTTP and jwt.

--- passport.js
+++ passport.js
@@ -7,4 +7,36 @@ let Users = Models.User,
     JWTStrategy = passportJWT.Strategy,
     ExtractJWT = passportJWT,ExtractJWT;
 
-    
+passport.use(new LocalStrategy({
+    usernameField: 'Username',
+    passwordField: 'Password'
+}, (username, password, callback) => {
+    console.log(username + '  ' + password);
+    Users.findOne({ Username: username }, (error, user) => {
+        if (error) {
+            console.log(error);
+            return callback(error);
+        }
+        //No user found.
+        if (!user) {
+            console.log('incorrect username: ' + username);
+            return callback(null, false, {message: 'Incorrect username or password.'})
+        }
+
+        console.log('finished');
+        return callback(null, user);
+    });
+}));
+
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'your_jwt_secret'
+}, (jwtPayload, callback) => {
+        return Users.findById(jwtPayload._id)
+            .then((user) => {
+                return callback(null,user);
+            })
+            .catch((error) => {
+                return callback(error);
+            });
+}));
