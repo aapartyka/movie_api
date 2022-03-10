169d3dbe3c4bf00ff37e176ef3a2f5d5221a561a
aapartyka <76936962+aapartyka@users.noreply.github.com>
2022-03-10 23:20:42 +0100

    create userModels.js

---
+++ models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+let userSchema = mongoose.Schema({
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
+  Watchlist: {
+    Movies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
+  },
+});
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.Password);
+};
+
+let User = mongoose.model('User', userSchema);
+
+module.exports.User = User;
